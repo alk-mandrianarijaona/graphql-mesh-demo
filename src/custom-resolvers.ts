481e1386652e0e04b5226b882346f23ab14a054a
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-07-22 17:18:15 +0200

    feat: get gravatar profile image

--- src/custom-resolvers.ts
+++ src/custom-resolvers.ts
@@ -1,9 +1,18 @@
 import { Resolvers } from "./generated/mesh";
+const gravatar = require("gravatar.js");
 
 export const resolvers: Resolvers = {
   Vehicle: {
     isOwnedByAlkemics: () => {
       return Math.random() >= 0.5;
+    },
+  },
+  User: {
+    slackName: () => {
+      return "@thadeo";
+    },
+    gravatarUrl: (root) => {
+      return gravatar.url(root.username);
     },
   },
 };
